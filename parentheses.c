a4e5c5ae2747fcca4950f114d326ab744e2427c2
GriffinGoing <griffingoing@gmail.com>
2020-04-02 15:51:47 -0400

    Uploaded the wrong version *eyeroll*

--- parentheses.c
+++ parentheses.c
@@ -5,6 +5,21 @@
  * This program checks for valid parentheses pairs by storing open parentheses, and then examing the most recently stored type against the next-found closing parentheses. If the types match, the opening parentheses is removed, and if they do not, then the line is not valid. If the end of the line is reached and no opening parentheses remain, the line is valid. If any remain, they were unmatched and the line is thus invalid. 
  *
  */
+
+char* removeLastOpen(char* parentheses) {
+	 char *newParen = (char*)calloc((strlen(parentheses) - 1), sizeof(char));
+         printf("current last: %c\n", parentheses[strlen(parentheses)-1]);
+	 parentheses[strlen(parentheses)-1] = 0;
+	 printf("paren after removal: %s\n", parentheses);
+	 strcpy(newParen, parentheses);
+	 //newParen[strlen(newParen)] = 0; //null character trickery
+	 printf("newParen after copy: %s\n", newParen);
+	 parentheses = (char*)calloc(strlen(newParen), sizeof(char));
+	 strcpy(parentheses, newParen);
+         free(newParen);
+	 return parentheses;
+}
+
 int main() {
 	
 	int charsInLine = 0; //characters in the line being examined
@@ -19,21 +34,24 @@ int main() {
 		
 		//printf("%s", line);
 		//char c = line[0];
+		int unmatched = 0; 
 		int length = strlen(line); //length of the current line
-		char paren[] = "";	
-		for(int i = 0; i < length; i++) {
-			char c = line[i];
+		char* paren = "";	
+		for(int k = 0; k < length; k++) {
+			char c = line[k];
 
 			//skip new line and terminating characters
 			if(c == '\n' || c =='\0') continue;
 
 			printf("%c\n", c);
 			
+			//if the current char is an opening parentheses, save it to
+			//the string of open parentheses
 			if(c == '(' | c == '{' | c == '[') {
-				printf("IN\n");
+				//printf("IN\n");
 				numOpenPars++;
 				int lenParen = strlen(paren);
-				printf("numOpenPars: %d\n", numOpenPars);
+				//printf("numOpenPars: %d\n", numOpenPars);
 
 				//add opening parentheses to paren string
 				int len = strlen(paren);
@@ -41,38 +59,71 @@ int main() {
 				strcpy(newParen, paren);
 				newParen[lenParen] = c;
 				newParen[lenParen+1] = 0;
-				printf("newParen: %s\n", newParen);
+				//printf("newParen: %s\n", newParen);
 				//printf("ints of newParen: %d\n", newParen);
-				strcpy(paren, newParen); //seg fault here
+				paren = (char*)calloc(strlen(newParen), sizeof(char));
+				strcpy(paren, newParen);
 				free(newParen);
-				printf("paren: %s\n", paren);
-				for(int j = 0; j < strlen(paren); j++) {
+				//printf("paren: %s\n", paren);
+				/*for(int j = 0; j < strlen(paren); j++) {
 					printf("%d\n", paren[j]);
-				}
+				}*/
 
 				//printf("%s\n", paren);
 			}
 			//printf("%c\n", c);
+			
+			//if the current char is a closing parentheses, check its type
+			int lenParen = strlen(paren);
+
 			switch(c) {
 				case ')':
-					printf("its a )");
-					numOpenPars--;
+					//printf("its a )\n");
+					printf("paren before: %s\n", paren);
+					if(paren[lenParen-1] == '(') {
+						paren = removeLastOpen(paren);
+						numOpenPars--;
+						printf("paren now: %s\n", paren);
+					}
+					else {
+						printf("NOT VALID");
+						unmatched = 1;
+					}
 					break;
 				case '}':
-					printf("its a }");
-					numOpenPars--;
+					//printf("its a }");
+					if(paren[lenParen-1] == '{') {
+                                                paren = removeLastOpen(paren);
+                                                numOpenPars--;
+                                                printf("paren now: %s\n", paren);
+                                        }
+                                        else { 
+						printf("NOT VALID");
+						unmatched = 1;
+					}
 					break;
 				case  ']':
-					printf("its a ]");
-					numOpenPars--;
+					//printf("its a ]");
+					if(paren[lenParen-1] == '[') {
+                                                paren = removeLastOpen(paren);
+                                                numOpenPars--;
+                                                printf("paren now: %s\n", paren);
+                                        }
+                                        else { 
+						printf("NOT VALID");
+						unmatched = 1;
+					}
 					break;
 				default:
 					break;
 			}
+
 		}
 
-		if(numOpenPars == 0) printf("VALID\n");
-		
+		if (unmatched == 0) {
+			if(numOpenPars == 0) printf("VALID");
+			else printf("NOT VALID");
+		}
 		numOpenPars = 0; //reset count for next line
 		charsInLine = getline(&line, &charsInLine, stdin); //get next line
 		
